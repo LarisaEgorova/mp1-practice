5d69e1d4ca4f5fd950fb2f866ea1a8e2cb959643
LarisaEgorova <egorovalarisa1326@gmail.com>
2023-10-17 21:18:13 +0300

    Final code

--- EgorovaLA/Practice1/Practice1/main.c
+++ EgorovaLA/Practice1/Practice1/main.c
@@ -2,17 +2,17 @@
 #include <locale.h>
 
 int main() {
-	const int DSP = 650, DVP = 950, WOOD = 660; // Плотность ДСП/ Плотность ДВП/ Плотность дерева в кг/м3
+	const int DSP = 650, DVP = 950, WOOD = 660; // Плотность ДСП/ ДВП/ дерева в кг/м3
 	const double DisMP = 0.4;
-	const double DSPT = 0.015, DVPT = 0.005, WOODT = 0.01; // Толщина определенных материалов 
-	double h, w, d, Massa, hbok, hbokf;
+	const double DSPT = 0.015, DVPT = 0.005, WOODT = 0.01; // Толщина 
+	double h, w, d, Massa, hf;
 	int n = 0;
 	int flag = 0;
 	setlocale(LC_ALL, "Rus");
 	while (flag == 0) {
 		printf("Введите высоту (180-220см) задней стенки \n");
 		scanf("%lf", &h);
-		if (h < 180 || h>220) printf("Некорректная высота, введите значение из диапазона указанного в скобках\n");
+		if (h < 180 || h>220) printf("Неверная высота, введите значение из скобок\n");
 		else flag = 1;
 	}
 	flag = 0;
@@ -20,7 +20,7 @@ int main() {
 	{
 		printf("Введите ширину (80-120см) задней стенки \n");
 		scanf("%lf", &w);
-		if (w < 80 || w>120) printf("Некорректная ширина, введите значение из диапазона указанного в скобках\n");
+		if (w < 80 || w>120) printf("Неверная ширина, введите значение из скобок\n");
 		else flag = 1;
 	}
 	flag = 0;
@@ -28,15 +28,14 @@ int main() {
 	{
 		printf("Введите глубину боковины (50-90)\n");
 		scanf("%lf", &d);
-		if (d < 50 || d>90) printf("Некорректная глубина, введите значение из диапазона указанного в скобках\n");
+		if (d < 50 || d>90) printf("Неверная глубина, введите значение из скобок\n");
 		else flag = 1;
 	}
 	h = h / 100; w = w / 100; d = d / 100;//перевод в метры
-	hbok = h - 2 * DSPT;
-	hbokf = hbok;
-	while (hbokf > DisMP)
+	hf = h;
+	while (hf > DisMP)
 	{
-		hbokf -= DisMP;
+		hf = hf-(DisMP+DSPT);
 		n++;
 	}
 	Massa = (h * w * DVPT * DVP) + //масса задней стенки
