83617f86b67827d838290bc1dc0bdf592e80f897
unknown <ITMM-230041@Q113.local>
2023-12-08 14:10:07 +0300

    Final code

--- EgorovaLA/Practice4/Practice4/main.c
+++ EgorovaLA/Practice4/Practice4/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <locale.h>
-#include <time.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -12,7 +11,7 @@ int i = 0;
 int flag = 1;
 int mode = 0;
 int bill[max_bill];
-float final_price = 0, price_dis[max_bill], final_price_no_dis = 0;;
+float final_price = 0.f, price_dis[max_bill], final_price_no_dis = 0.f;
 int pos = 1;
 
 struct product
@@ -23,20 +22,23 @@ struct product
     int discount;
 };
 
-struct product product[] = { {"0000", "Хлеб", 40, 10}, {"0001", "Молоко 0,9 л.", 74, 15},
-   {"0011", "Крупа гречневая", 101,3},{"0111", "Кекс", 25,6},
-   {"1111", "Яйцо куриное", 90, 9}, {"1000", "Сосиски сливочные", 156, 20},
-   {"1100", "Картофель фас.", 41, 2},{"1110", "Шампунь", 251, 50},
-   {"1010","Вода негаз. 0,5 л.", 15, 1}, {"0101", "Консервы рыбные", 113, 12} };
+struct product products[] = { 
+   {"0000", "Хлеб", 40.f, 10}, {"0001", "Молоко 0,9 л.", 74.f, 15},
+   {"0011", "Крупа гречневая", 101.f, 3},{"0111", "Кекс", 25.f, 6},
+   {"1111", "Яйцо куриное", 90.f, 9}, {"1000", "Сосиски сливочные", 156.f, 20},
+   {"1100", "Картофель фас.", 41.f, 2},{"1110", "Шампунь", 251.f, 50},
+   {"1010","Вода негаз. 0,5 л.", 15.f, 1},
+   {"0101", "Консервы рыбные", 113.f, 12}
+};
 
 int chek_product() {
     int i, j, n=1, position=0;
-    char barcode_pols[len_bar+1];
+    char barcode_pols[10];
     do {
         printf("Введите штрих код ");
-        scanf("%s", &barcode_pols);
+        scanf("%s", barcode_pols);
         for (i = 0; i < k; i++) {
-            n = strcmp(product[i].barcode, barcode_pols);
+            n = strcmp(products[i].barcode, barcode_pols);
             if (n == 0) {
                 position = i;
                 break;
@@ -52,30 +54,41 @@ int chek_product() {
 int start() {
     setlocale(LC_ALL, "rus");
    
-    printf("Вас приветствует кассовый аппарат '10023', начнем работу\n");
+    printf("Вас приветствует кассовый аппарат '10023'\n");
+    printf("Начнем работу\n");
     printf("Отсканируйте первый товар\n");
-    bill[0] = chek_product(&product);
+    bill[0] = chek_product();
   
-    final_price_no_dis = final_price_no_dis + product[bill[0]].prices;
-    price_dis[0] = (product[bill[0]].prices) * (((float)(100 - product[bill[0]].discount)) / 100);
-    final_price = final_price + price_dis[0];
+    final_price_no_dis += products[bill[0]].prices;
+    price_dis[0] = (products[bill[0]].prices) *
+        (((float)(100 - products[bill[0]].discount)) / 100);
+    final_price += price_dis[0];
 
-    printf("%s %.2f руб. %d процентов скидка\n", product[bill[0]].names, product[bill[0]].prices, product[bill[0]].discount);
+    printf("%s %.2f руб. %d процентов скидка\n", products[bill[0]].names,
+        products[bill[0]].prices, products[bill[0]].discount);
 
     while (flag) {
         do {
-            printf("Выберете режим. (1) Сканировать новый товар. (2) Закрыть чек\n");
+            printf("Выберете режим.\n");
+            printf("(1) Сканировать новый товар. (2) Закрыть чек\n");
             scanf("%d", &mode);
-            if (mode != 1 && mode != 2) printf("Выберете один из доступных режимов\n");
+            if (mode != 1 && mode != 2)
+                printf("Выберете один из доступных режимов\n");
         } while (mode != 1 && mode != 2);
 
         switch (mode) {
         case 1:
-            bill[pos] = chek_product(&product);
-            printf("%s %.2f руб. %d процентов скидка\n", product[bill[pos]].names, product[bill[pos]].prices, product[bill[pos]].discount);
-            final_price_no_dis = final_price_no_dis + product[bill[pos]].prices;
-            price_dis[pos] = (product[bill[pos]].prices) * ( ((float)(100 - product[bill[pos]].discount)) / 100);
-            final_price = final_price + price_dis[pos];
+            bill[pos] = chek_product();
+            printf("%s %.2f руб. %d процентов скидка\n",
+                products[bill[pos]].names, products[bill[pos]].prices,
+                products[bill[pos]].discount);
+
+            final_price_no_dis += products[bill[pos]].prices;
+
+            price_dis[pos] = (products[bill[pos]].prices) *
+                (((float)(100 - products[bill[pos]].discount)) / 100);
+
+            final_price += price_dis[pos];
             pos++;
             break;
         case 2:
@@ -88,7 +101,8 @@ int start() {
 int print_bill() {
     printf("Ваш чек:\n");
     for (i = 0; i < pos; i++) {
-        printf("%s %.2f руб. со скидкой %.2f руб.\n", product[bill[i]].names, product[bill[i]].prices, price_dis[i]);
+        printf("%s %.2f руб. со скидкой %.2f руб.\n", 
+            products[bill[i]].names, products[bill[i]].prices, price_dis[i]);
     }
     printf("\n");
     printf("Итоговая сумма без скидки %.2f руб.\n", final_price_no_dis);
@@ -102,7 +116,8 @@ void main() {
             print_bill();
             printf("Пробиваем следущий чек? (1) - да, (0) - нет\n");
             scanf("%d", &flag);
-            if (flag != 0 && flag != 1) printf("Выберете один из доступных режимов\n");
+            if (flag != 0 && flag != 1)
+                printf("Выберете один из доступных режимов\n");
         } while (flag != 0 && flag != 1);
     }
     return;
